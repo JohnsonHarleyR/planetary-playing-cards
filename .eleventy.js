f6af8641cce5251fd637893945f833f6125583f2
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2025-02-10 04:39:57 +0000

    Got the basic setup for the cards.

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,15 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addShortcode("card", function(planet, au, size) {
+    let idPlanet = planet.toLowerCase();
+    let title = size == 'lg' ? `<h2>${planet}</h2>` : `<h3>${planet}</h3>`;
+    return `
+    <div id="${idPlanet}" class="card ${size}">
+      <div id="${idPlanet}-icon" class="icon"></div>
+      ${title}
+      <p>${au}</p>
+    </div>
+    `;
+  });
 };
